2f338f488f8720edae605e6e3a06f297fe2c2896
JJ <jj.han@live.com>
2020-07-27 02:29:48 +0900

    First Commit - Algorithm to find max sum rectangle in matrix with Kadane's algorithm - TODO: to implement the same method finding indices, corners of max sum rectangle

---
+++ src/com/jj/practice/MaxRectangle.java
@@ -0,0 +1,47 @@
+package com.jj.practice;
+
+// https://practice.geeksforgeeks.org/problems/maximum-sum-rectangle/0
+// https://www.geeksforgeeks.org/maximum-sum-rectangle-in-a-2d-matrix-dp-27/
+
+public class MaxRectangle {
+    public static void main(String[] args) {
+        int[][] test1 = {   {1, 2, -1, -4, -20},        // 29
+                            {-8, -3, 4, 2, 1},
+                            {3, 8, 10, 1, 3},
+                            {-4, -1, 1, 7, -6} };
+        int[][] test2 = {   {-10, -21, 5},              // 55
+                            {-15, 4, 13},
+                            {17, -16, -4},
+                            {24, 0, -7},
+                            {-4, -22, -6},
+                            {-20, -12, -12},
+                            {12, 24, 18},
+                            {-21, 7, -9},
+                            {-6, 24, -11} };
+        System.out.println(getMaxRectangle(test1));
+        System.out.println(getMaxRectangle(test2));
+    }
+
+    public static int getMaxRectangle(int[][] a) {
+        int r = a.length, c = a[0].length, max = a[0][0];
+        for(int i = 0; i < r; i++) {
+            int[] sum = new int[c];
+            for (int j = i; j < r; j++) {
+                for (int k = 0; k < c; k++)
+                    sum[k] += a[j][k];
+                max = Math.max(max, getMaxSum(sum));
+            }
+        }
+        return max;
+    }
+    private static int getMaxSum(int[] a) {
+        int max = a[0], currMax = a[0];
+        for(int i = 1; i < a.length; i++) {
+            currMax = Math.max(a[i], a[i] + currMax);
+            max = Math.max(max, currMax);
+        }
+        return max;
+    }
+
+// TODO: to implement the same method finding indices, left top and right bottom corners of rectangle.
+}
