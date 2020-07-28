57709e9c6b16138dd6e348a642824cffc4e875ce
JJ <jj.han@live.com>
2020-07-29 00:28:15 +0900

    * changed parameter implement 'from' and 'to' values to 0-based index Now, these parameters represent index in array. from : 0, to : 3 => range from array[0] to array[3]
    * improved segment tree size calculation
    size now takes 2 * 2 ^ ceil[log2(n)], where n is original array size

--- src/com/jj/practice/SegmentTree.java
+++ src/com/jj/practice/SegmentTree.java
@@ -2,7 +2,7 @@ package com.jj.practice;
 
 import java.util.Arrays;
 
-// Segment Tree for Sum (1 based Index Array)
+// Segment Tree for Sum
 // No Modification Method...
 // TODO: to implement update method
 // TODO: to implement segment tree for min value
@@ -14,8 +14,8 @@ public class SegmentTree {
 
     SegmentTree(int[] array) {
         this.array = Arrays.copyOf(array, array.length);
-        //max size = 2 * 2 ^ log2(n) + 1
-        size = (int) (2 * Math.pow(2.0, Math.floor((Math.log((double) array.length) / Math.log(2.0) ) + 1)));
+        //max size = 2 * 2 ^ ceil[log2(n)] for 1-based index array
+        size = (int) (2 * Math.pow(2, (Math.ceil(Math.log(array.length) / Math.log(2)))));
         st = new int[size];
         build(1, 0, array.length);
     }
@@ -23,6 +23,7 @@ public class SegmentTree {
     public int size() {
         return array.length;
     }
+    public int sizeST() { return size; }
 
     private void build(int v, int from, int size) {
         if(size == 1)   st[v] = array[from];
@@ -33,14 +34,19 @@ public class SegmentTree {
         }
     }
 
+    /**
+     * Range Sum Query
+     * @param  from from index
+     * @param  to to index
+     */
     public int rsq(int from, int to) {
-        if(from < 1 || to > array.length || from > to)  throw new IllegalArgumentException("invalid argument");
-        return rsq(1, 1, array.length, from, to);
+        if(from < 0 || to >= array.length || from > to)  throw new IllegalArgumentException("invalid argument");
+        return rsq(1, 0, array.length-1, from, to);
     }
     private int rsq(int v, int lo, int hi, int from, int to) {
         if(from <= lo && hi <= to)  return st[v];
         if(from > hi || lo > to)    return 0;
-        int mid = lo + (hi-1-lo)/2;            //
+        int mid = lo + (hi-lo-1)/2;
         int leftSum = rsq(2*v, lo, mid, from, to);
         int rightSum = rsq(2*v + 1, mid+1, hi, from, to);
         return leftSum + rightSum;
@@ -49,15 +55,17 @@ public class SegmentTree {
     public void print() {
         for(int i = 1; i < size; i++)
             System.out.print(st[i] + " ");
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 7, 9, 11};
         int[] arr2 = {1, 3, 5, 7};
         SegmentTree st = new SegmentTree(arr);
-        //st.print();
-        for(int i = 1; i <= arr.length; i++)
-            for(int j = i; j <= arr.length; j++)
+        System.out.println(st.sizeST());
+        st.print();
+        for(int i = 0; i < st.size(); i++)
+            for(int j = i; j < st.size(); j++)
                 System.out.println(i + " " + j + ": " + st.rsq(i, j));
     }
 }
