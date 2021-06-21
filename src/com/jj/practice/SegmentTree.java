d183bb3413764e6e6bcd0659931215193d50dbcd
neoteny86 <jj.han@live.com>
2021-06-20 23:53:54 -0400

    updated: SegmentTree.java - eager update implement

--- src/com/jj/practice/SegmentTree.java
+++ src/com/jj/practice/SegmentTree.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 // Segment Tree for Sum
 // No Modification Method...
-// TODO: to implement update method
 // TODO: to implement segment tree for min value
 
 public class SegmentTree {
@@ -14,7 +13,8 @@ public class SegmentTree {
 
     SegmentTree(int[] array) {
         this.array = Arrays.copyOf(array, array.length);
-        //max size = 2 * 2 ^ ceil[log2(n)] for 1-based index array
+        // max size = 2 * 2 ^ ceil[log2(n)] for 1-based index array
+        // safest min value array.size x 4
         size = (int) (2 * Math.pow(2, (Math.ceil(Math.log(array.length) / Math.log(2)))));
         st = new int[size];
         build(1, 0, array.length);
@@ -34,11 +34,6 @@ public class SegmentTree {
         }
     }
 
-    /**
-     * Range Sum Query
-     * @param  from from index
-     * @param  to to index
-     */
     public int rsq(int from, int to) {
         if(from < 0 || to >= array.length || from > to)  throw new IllegalArgumentException("invalid argument");
         return rsq(1, 0, array.length-1, from, to);
@@ -46,10 +41,28 @@ public class SegmentTree {
     private int rsq(int v, int lo, int hi, int from, int to) {
         if(from <= lo && hi <= to)  return st[v];
         if(from > hi || lo > to)    return 0;
+        // reason of hi-lo-1 => when node size is odd, left node(2*v) is always smaller size than right node(2*v +1) since size/2 from build method
         int mid = lo + (hi-lo-1)/2;
         int leftSum = rsq(2*v, lo, mid, from, to);
         int rightSum = rsq(2*v + 1, mid+1, hi, from, to);
         return leftSum + rightSum;
+    }
+
+    // eager update implement which updates result immediately
+    // TODO: laze update implement with node segment tree structure
+    public void update(int idx, int val){
+        int v = 1, lo = 0, hi = array.length-1, diff = val - array[idx];
+        while (lo < hi) {
+            st[v] += diff;
+            int mid = lo + (hi - lo - 1)/2;
+            v *= 2;
+            if (idx <= mid) hi = mid;
+            else {
+                lo = mid + 1;
+                v++;
+            }
+        }
+        array[idx] = st[v] = val;
     }
 
     public void print() {
